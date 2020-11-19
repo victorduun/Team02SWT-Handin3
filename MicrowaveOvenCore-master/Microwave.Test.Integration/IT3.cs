b24ead3cd46b61c4a177ee0e721508de15debc0f
Tjsaaby <Tjsaaby@outlook.dk>
2020-11-19 14:20:01 +0100

    Resolving review points from Frank and moving current test to IT2 and create new for interface of interest

--- MicrowaveOvenCore-master/Microwave.Test.Integration/IT3.cs
+++ MicrowaveOvenCore-master/Microwave.Test.Integration/IT3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Microwave.Classes.Boundary;
 using Microwave.Classes.Controllers;
@@ -29,7 +30,7 @@ namespace Microwave.Test.Integration
         private IDisplay _display;
         private IPowerTube _powerTube;
         private ILight _light;
-        private ITimer _timer;
+        private ITimer _faketimer;
         private IOutput _fakeOutput;
         private UserInterface _uut;
         private CookController _cooker;
@@ -41,48 +42,67 @@ namespace Microwave.Test.Integration
             _fakestartCancelButton = Substitute.For<IButton>();
             _fakeOutput = Substitute.For<IOutput>();
             _fakedoor = Substitute.For<IDoor>();
+            _faketimer = Substitute.For<ITimer>();  //Using fake timer as the integration test would otherwise run for a long time
+
             _display = new Display(_fakeOutput);
             _powerTube = new PowerTube(_fakeOutput);
             _light = new Light(_fakeOutput);
-            _timer = new Timer();
-            _cooker = new CookController(_timer, _display, _powerTube);
+            _cooker = new CookController(_faketimer, _display, _powerTube);
             _uut = new UserInterface(_fakepowerButton, _faketimeButton, _fakestartCancelButton, _fakedoor, _display, _light, _cooker);
+            _cooker.UI = _uut;  // Finish the double association
+
         }
 
-        [Test]
-        public void OpenDoor_LightOn_LogEqualOn()
+        /* Initiate Start cooking helper function*/
+        public void IT_SetupToCook(int setPower, int setTime)
         {
             _uut.OnDoorOpened(this,EventArgs.Empty);
-            _fakeOutput.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("on")));
-        }
-
-        [Test]
-        public void CloseDoor_LightOff_LogEqualOff()
-        {
-            _uut.OnDoorOpened(this, EventArgs.Empty);
             _uut.OnDoorClosed(this, EventArgs.Empty);
-            _fakeOutput.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("off")));
+
+            for (int i = 0; i < setPower; i++)
+            {
+                _uut.OnPowerPressed(this, EventArgs.Empty);
+            }
+
+            for (int i = 0; i < setTime; i++)
+            {
+                _uut.OnTimePressed(this, EventArgs.Empty);
+            }
+
+        }
+     
+        [Test]
+        public void CookingIsStarted_DisplayShow_PowerTubeShow_100()
+        {
+            //Set power to 100 and set time to 1min
+            IT_SetupToCook(2,1);
+            _uut.OnStartCancelPressed(this,EventArgs.Empty);
+            _fakeOutput.Received(2).OutputLine(Arg.Is<string>(str => str.Contains("100")));
         }
 
         [Test]
-        public void PressButton_Power_LogEqual50()
+        public void CookingIsStarted_DisplayShow_PowerTubeShow_100_ShowTime_1()
         {
-            _uut.OnPowerPressed(this, EventArgs.Empty);
-            _fakeOutput.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("50")));
-        }
-
-        [Test]
-        public void PressButton_Time_LogEqual()
-        {
-            _uut.OnTimePressed(this, EventArgs.Empty);
-            _fakeOutput.Received(1).OutputLine(Arg.Is<string>(str => str.Contains("1:00")));
-        }
-
-        [Test]
-        public void PressButton_StartCancel_LogEqual()
-        {
+            //Set power to 100 and set time to 1min
+            IT_SetupToCook(1, 1);
             _uut.OnStartCancelPressed(this, EventArgs.Empty);
-            _fakeOutput.Received(0).OutputLine(Arg.Is<string>(str=>str.Contains(null)));
+            _fakeOutput.Received(3).OutputLine(Arg.Is<string>(str => str.Contains("1")));
         }
+
+        [Test]
+        public void CookingIsStarted_DisplayShow_PowerTubeShow_100_ShowTime1()
+        {
+                        //Set power to 100 and set time to 1min
+            IT_SetupToCook(2, 1);
+            _uut.OnStartCancelPressed(this, EventArgs.Empty);
+            _fakeOutput.ClearReceivedCalls();
+            _faketimer.Expired += Raise.EventWith<EventArgs>();
+            _fakeOutput.Received(3).OutputLine(Arg.Is<string>(str =>
+                str.ToLower().Contains("powertube turned off") &&
+                str.ToLower().Contains("display cleared") &&
+                str.ToLower().Contains("light is turned off")
+            ));
+        }
+
     }
 }
