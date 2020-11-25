3c00fcd41290dfec659cf14d205a3b5db66dc20a
ChristianKrog <christiankrog@hotmail.dk>
2020-11-25 09:55:40 +0100

    Merge fix

---
+++ MicrowaveOvenCore-master/Microwave.Test.Integration/IT4.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT4
+    {
+        private IButton _fakepowerButton;
+        private IButton _faketimeButton;
+        private IButton _startCancelButton;
+        private IDoor _fakedoor;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ILight _light;
+        private ITimer _fakeTimer;
+        private IOutput _fakeOutput;
+        private UserInterface _uut;
+        private CookController _cooker;
+        [SetUp]
+        public void Setup()
+        {
+            _fakepowerButton = Substitute.For<IButton>();
+            _faketimeButton = Substitute.For<IButton>();
+            _startCancelButton = new Button();
+            _fakeOutput = Substitute.For<IOutput>();
+            _fakedoor = Substitute.For<IDoor>();
+            _display = new Display(_fakeOutput);
+            _powerTube = new PowerTube(_fakeOutput);
+            _light = new Light(_fakeOutput);
+            _fakeTimer = Substitute.For<ITimer>();
+            _cooker = new CookController(_fakeTimer, _display, _powerTube);
+            _uut = new UserInterface(_fakepowerButton, _faketimeButton, _startCancelButton, _fakedoor, _display, _light, _cooker);
+            //Finish double association
+            _cooker.UI = _uut;
+        }
+
+        [Test]
+        public void StartCancelButtonPressed_TimerStartedWithCorrectParameters_TimerStarted()
+        {
+            _fakepowerButton.Pressed += Raise.EventWith<EventArgs>();
+            _faketimeButton.Pressed += Raise.EventWith<EventArgs>();
+            _startCancelButton.Press();
+            _fakeTimer.Received().Start(60);
+        }
+
+        [Test]
+        public void StartCancelButtonPressed_OutputIsCorrect_OutputIsCorrect()
+        {
+            _fakepowerButton.Pressed += Raise.EventWith<EventArgs>();
+            _faketimeButton.Pressed += Raise.EventWith<EventArgs>();
+            _fakeOutput.ClearReceivedCalls();
+            _startCancelButton.Press();
+
+            _fakeOutput.Received(2).OutputLine(Arg.Any<string>());
+        }
+
+    }
+}
---
+++ MicrowaveOvenCore-master/Microwave.Test.Integration/IT6.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT6
+    {
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _startCancelButton;
+        private IDoor _fakedoor;
+        private IDisplay _display;
+        private IPowerTube _powerTube;
+        private ILight _light;
+        private ITimer _fakeTimer;
+        private IOutput _fakeOutput;
+        private UserInterface _uut;
+        private CookController _cooker;
+        [SetUp]
+        public void Setup()
+        {
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _fakeOutput = Substitute.For<IOutput>();
+            _fakedoor = Substitute.For<IDoor>();
+            _display = new Display(_fakeOutput);
+            _powerTube = new PowerTube(_fakeOutput);
+            _light = new Light(_fakeOutput);
+            _fakeTimer = Substitute.For<ITimer>();
+            _cooker = new CookController(_fakeTimer, _display, _powerTube);
+            _uut = new UserInterface(_powerButton, _timeButton, _startCancelButton, _fakedoor, _display, _light, _cooker);
+            //Finish double association
+            _cooker.UI = _uut;
+        }
+
+        [Test]
+        public void StartCancelButtonPressed_TimerStartedWithCorrectParameters_TimerStarted()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _startCancelButton.Press();
+            _fakeTimer.Received().Start(60);
+        }
+
+        [Test]
+        public void StartCancelButtonPressed_OutputIsCorrect_OutputIsCorrect()
+        {
+            _powerButton.Press();
+            _timeButton.Press();
+            _fakeOutput.ClearReceivedCalls();
+            _startCancelButton.Press();
+
+            _fakeOutput.Received(2).OutputLine(Arg.Any<string>());
+        }
+
+    }
+}
