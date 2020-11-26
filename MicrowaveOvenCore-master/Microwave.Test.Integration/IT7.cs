e73aab1114f3e423fd2ce2cc07eb83de95279ff2
ChristianKrog <christiankrog@hotmail.dk>
2020-11-26 12:25:54 +0100

    Adding integration test file for IT7

---
+++ MicrowaveOvenCore-master/Microwave.Test.Integration/IT7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microwave.Classes.Boundary;
+using Microwave.Classes.Controllers;
+using Microwave.Classes.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class IT7
+    {        
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+    }
+}
